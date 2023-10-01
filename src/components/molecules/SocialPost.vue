1d3b6a95a28f60f3c5072d7d50e62b6ebb577fb6
Zelig880 <scuomo1988@gmail.com>
2023-10-01 10:12:36 +0100

    Initial Commit

---
+++ src/components/molecules/SocialPost.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="SocialPost">
+    <div class="header">
+      <img class="avatar" src="https://i.pravatar.cc/40" />
+      <div class="name">Name of User</div>
+      <div class="userId">@userId</div>
+    </div>
+    <div class="post">This is a dummy post</div>
+  </div>
+</template>
+
+<script setup >
+</script>
+
+<style lang="scss">
+.SocialPost{
+  .header {
+    display: flex;
+    align-items: center;
+    margin-bottom: 8px;;
+  }
+  .avatar {
+    border-radius: 50%;
+    margin-right: 12px;
+  }
+  .name {
+    font-weight: bold;
+    margin-right: 8px;
+    color: white;
+  }
+}
+</style>
