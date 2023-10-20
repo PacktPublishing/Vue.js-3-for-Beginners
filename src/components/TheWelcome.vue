418864d9bcddac43b204ee099eb73ce5fc4422fd
Zelig880 <scuomo1988@gmail.com>
2023-10-20 20:39:47 +0100

    Chapter 3

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,3 +1,33 @@
 <template>
-  <div></div>
+  <SocialPost
+    :username="posts[0].username"
+    :userId="posts[0].userId"
+    :avatarSrc="posts[0].avatar"
+    :post="posts[0].post"
+  ></SocialPost>
+  <SocialPost
+    :username="posts[1].username"
+    :userId="posts[1].userId"
+    :avatarSrc="posts[1].avatar"
+    :post="posts[1].post"
+  ></SocialPost>
 </template>
+
+<script setup>
+import { reactive } from 'vue';
+import SocialPost from './molecules/SocialPost.vue'
+
+const posts = reactive([
+  { username: "Username one",
+    userId: "usernameId1", 
+    avatar: "https://i.pravatar.cc/40",
+    post: "This is my post"
+  },
+  { username: "Username two",
+    userId: "usernameId2", 
+    avatar: "https://i.pravatar.cc/40",
+    post: "This is my second post"
+  }
+]);
+
+</script>
---
+++ src/components/molecules/SocialPost.vue
@@ -0,0 +1,53 @@
+<template>
+  <div
+    class="SocialPost"
+    :class="{ SocialPost__selected: selected}"
+    @click="selected = !selected"
+  >
+    <div class="header">
+      <img class="avatar" :src="avatarSrc" />
+      <div class="name">{{ username }}</div>
+      <div class="userId">{{ userId }}</div>
+    </div>
+    <div class="post">{{ post }}</div>
+  </div>
+</template>
+
+<script setup >
+import { onMounted, ref } from 'vue';
+
+const selected = ref(false);
+
+const props = defineProps({
+  username: String,
+  userId: Number,
+  avatarSrc: String,
+  post: String
+});
+
+onMounted( () => {
+  console.log(props.username);
+});
+</script>
+
+<style lang="scss">
+.SocialPost{
+  &__selected{
+    border: white solid 1px;
+  }
+  .header {
+    display: flex;
+    align-items: center;
+    margin-bottom: 8px;;
+  }
+  .avatar {
+    border-radius: 50%;
+    margin-right: 12px;
+  }
+  .name {
+    font-weight: bold;
+    margin-right: 8px;
+    color: white;
+  }
+}
+</style>
