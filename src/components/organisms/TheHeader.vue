211a6f367b57eae886ddb5394656d4cb7364a3c1
Zelig880 <scuomo1988@gmail.com>
2023-10-20 21:09:57 +0100

    Chapter 4 - start

---
+++ src/components/organisms/TheHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <img src="https://picsum.photos/50/50" />
+        <h1>Companion app</h1>
+        <a href="#">
+            Welcome 
+            <span>Zelig880</span>
+        </a>
+    </header>
+</template>
+<script setup>
+</script>
+<style lang="scss">
+header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    grid-column-start: 1;
+    grid-column-end: 3;
+    margin-bottom:24px;
+
+    a span {
+        font-weight: bold;
+    }
+}
+
+</style>
